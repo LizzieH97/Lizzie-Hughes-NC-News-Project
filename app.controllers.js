ad29aac9a3d40c3aa1482e8514e2cfa2a45277e9
LizzieH97 <erhughes97@gmail.com#>
2024-08-27 14:27:53 +0100

    spent AGES trying to find the error to find out it's just a 404 error at this stage, also added corrections for earlier in the task

--- app.controllers.js
+++ app.controllers.js
@@ -4,9 +4,9 @@ exports.getTopics = (req, res, next) => {
   
     findTopics(req)
     .then((topicsArr) => {
-        res.status(200).send(topicsArr)
+        res.status(200).send({topicsArr})
     })
     .catch((err) => {
-        console.log(err)
+        next(err)
     });
 }
